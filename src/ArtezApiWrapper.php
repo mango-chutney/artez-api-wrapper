3b1a4a4f75b0a1e7ef3dbb3e2ec92a7cd827cc1c
Jason Di Benedetto <jason.dibenedetto6@gmail.com>
2022-05-11 16:29:27 +0930

    hello world

--- src/ArtezApiWrapper.php
+++ src/ArtezApiWrapper.php
@@ -1,10 +1,19 @@
 <?php
 
-namespace MangoChutney\ArtezAPIWrapper;
+namespace MangoChutney\ArtezApiWrapper;
 
-class ArtezAPIWrapper
+use Klein;
+
+class ArtezApiWrapper
 {
     public function start()
     {
+        $klein = new Klein();
+
+        $klein->with('/regapi', function () use ($klein) {
+            $klein->respond('GET', '/Constituents', function ($request, $response) {
+                print_r('Hello World');
+            });
+        });
     }
 }
