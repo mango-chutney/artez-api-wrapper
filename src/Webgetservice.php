196e929cfb764b0f46f9d1ef3e941d5d068945b8
yofrancisco <yofranciscomartinez@gmail.com>
2023-09-27 15:52:52 +0930

    webgetservice functions

--- src/Webgetservice.php
+++ src/Webgetservice.php
@@ -9,16 +9,24 @@ class Webgetservice extends Api
         $group = $this->config['path'];
 
         $this->router->addGroup($group.'/webgetservice', function (\FastRoute\RouteCollector $router) {
-            $router->addRoute('GET', '/get.asmx/teamByTeamID', function () {
-                $this->handleRoute('/webgetservice/get.asmx/teamByTeamID', ['languageCode', 'teamID']);
+            $router->addRoute('GET', '/get.asmx/getEventFundraisingTotals', function () {
+                $this->handleRoute('/webgetservice/get.asmx/getEventFundraisingTotals', ['eventID', 'loginOrgID', 'locationExportID', 'source']);
             });
 
-            $router->addRoute('GET', '/get.asmx/teamSearchReg', function () {
-                $this->handleRoute('/webgetservice/get.asmx/teamSearchReg', ['eventID', 'teamName', 'captainFirstName', 'captainLastName', 'eventLocationID', 'eventLocationStartTimeID']);
+            $router->addRoute('GET', '/get.asmx/getMessageBoard', function () {
+                $this->handleRoute('/webgetservice/get.asmx/getMessageBoard', ['teamID', 'registrantID', 'personID']);
             });
 
-            $router->addRoute('GET', '/get.asmx/postMessageBoardMain', function () {
-                $this->handleRoute('/webgetservice/get.asmx/postMessageBoardMain', ['corporateTeamID', 'eventID', 'locationID', 'personID', 'registrantID', 'teamID', 'author', 'messageText', 'langPref', 'source']);
+            $router->addRoute('GET', '/get.asmx/getParticipantScoreBoard', function () {
+                $this->handleRoute('/webgetservice/get.asmx/getParticipantScoreBoard', ['eventID', 'sortBy', 'listItemCount', 'externalQuestionID', 'externalAnswerID', 'languageCode', 'source']);
+            });
+
+            $router->addRoute('GET', '/get.asmx/getRegistrant', function () {
+                $this->handleRoute('/webgetservice/get.asmx/getRegistrant', ['registrantID', 'uniqueID', 'source']);
+            });
+
+            $router->addRoute('GET', '/get.asmx/getTeamScoreBoard', function () {
+                $this->handleRoute('/webgetservice/get.asmx/getTeamScoreBoard', ['eventID', 'sortBy', 'listItemCount', 'externalQuestionID', 'externalAnswerID', 'languageCode', 'source']);
             });
 
             $router->addRoute('POST', '/mobile.asmx/mobileParticipant', function () {
@@ -32,6 +40,18 @@ class Webgetservice extends Api
                 ];
 
                 $this->handleRoute('/webgetservice/mobile.asmx/mobileParticipant', null, $body, $options);
+            });
+
+            $router->addRoute('GET', '/get.asmx/postMessageBoardMain', function () {
+                $this->handleRoute('/webgetservice/get.asmx/postMessageBoardMain', ['corporateTeamID', 'eventID', 'locationID', 'personID', 'registrantID', 'teamID', 'author', 'messageText', 'langPref', 'source']);
+            });
+
+            $router->addRoute('GET', '/get.asmx/teamByTeamID', function () {
+                $this->handleRoute('/webgetservice/get.asmx/teamByTeamID', ['languageCode', 'teamID']);
+            });
+
+            $router->addRoute('GET', '/get.asmx/teamSearchReg', function () {
+                $this->handleRoute('/webgetservice/get.asmx/teamSearchReg', ['eventID', 'teamName', 'captainFirstName', 'captainLastName', 'eventLocationID', 'eventLocationStartTimeID']);
             });
         });
     }
